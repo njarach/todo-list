68ba22ba4bfa921532188c15ca757afb2d6487b9
nicol <nicolas.jarach@hotmail.com>
2025-05-09 12:32:22 +0200

    Fixed user management functionality and added delete action

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,22 +8,25 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\PasswordHasherInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class UserController extends AbstractController
 {
     #[Route('/users', name: 'users_list')]
+    #[IsGranted('ROLE_ADMIN')]
     public function listAction(UserRepository $userRepository): Response
     {
         return $this->render('user/list.html.twig', ['users' => $userRepository->findAll()]);
     }
 
     #[Route('/users/create', name: 'user_create')]
+    #[IsGranted('ROLE_ADMIN')]
     public function createAction(
         Request $request,
         UserRepository $userRepository,
-        PasswordHasherInterface $passwordHasher
+        UserPasswordHasherInterface $passwordHasher
     ): Response
     {
         $user = new User();
@@ -32,7 +35,8 @@ class UserController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $hashedPassword = $passwordHasher->hash(
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
                 $user->getPassword()
             );
             $user->setPassword($hashedPassword);
@@ -43,7 +47,7 @@ class UserController extends AbstractController
 
             $this->addFlash('success', "L'utilisateur a bien été ajouté.");
 
-            return $this->redirectToRoute('user_list');
+            return $this->redirectToRoute('users_list');
         }
 
         return $this->render('user/create.html.twig', [
@@ -52,14 +56,16 @@ class UserController extends AbstractController
     }
 
     #[Route('/users/{id}/edit', name: 'user_edit')]
-    public function editAction(User $user, UserRepository $userRepository, PasswordHasherInterface $passwordHasher, Request $request): \Symfony\Component\HttpFoundation\RedirectResponse|Response
+    #[IsGranted('ROLE_ADMIN')]
+    public function editAction(User $user, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher, Request $request): \Symfony\Component\HttpFoundation\RedirectResponse|Response
     {
         $form = $this->createForm(UserType::class, $user);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $hashedPassword = $passwordHasher->hash(
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
                 $user->getPassword()
             );
             $user->setPassword($hashedPassword);
@@ -70,9 +76,19 @@ class UserController extends AbstractController
 
             $this->addFlash('success', "L'utilisateur a bien été modifié");
 
-            return $this->redirectToRoute('user_list');
+            return $this->redirectToRoute('users_list');
         }
 
         return $this->render('user/edit.html.twig', ['form' => $form->createView(), 'user' => $user]);
+    }
+
+    #[Route('/users/{id}/delete', name: 'user_delete')]
+    public function deleteAction(User $user, UserRepository $userRepository): \Symfony\Component\HttpFoundation\RedirectResponse
+    {
+        $userRepository->remove($user, true);
+
+        $this->addFlash('success', "L'utilisateur a bien été supprimé.");
+
+        return $this->redirectToRoute('users_list');
     }
 }
